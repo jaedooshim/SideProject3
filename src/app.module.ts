5954e1400ad7045f50ae79aed8f96f1e2e4d5ec7
shimjaedoo <woen1637@naver.com>
2024-03-05 21:45:55 +0900

    chore : Module 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './_common/prisma/prisma.module';
 import { BcryptModule } from './_common/bcrypt/bcrypt.module';
 import { MemberModule } from './member/member.module';
+import { PostModule } from './post/post.module';
+import { BadwordsModule } from './badwords/badwords.module';
+import { CommentModule } from './post/comment/comment.module';
 
 @Module({
-  imports: [PrismaModule, BcryptModule, MemberModule],
+  imports: [PrismaModule, BcryptModule, MemberModule, PostModule, BadwordsModule, CommentModule],
 })
 export class AppModule {}
